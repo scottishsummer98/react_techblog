33a3b6aa855ed89904df14463e0761c5151cbee2
scottishsummer98 <samiremail98@gmail.com>
2023-06-08 10:26:47 +0600

    'footerdone'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./styles/App.css";
 import "./styles/Main.css";
 import "./styles/Sub.css";
-import Login from "./components/main/auth/Login"
-import Signup from "./components/main/auth/Signup"
+import Login from "./components/main/auth/Login";
+import Signup from "./components/main/auth/Signup";
 import Home from "./components/main/Home";
 import Footer from "./components/main/Footer";
 import Header from "./components/main/Header";
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Archives from "./components/main/Archives";
 import Career from "./components/main/Career";
 import Events from "./components/main/Events";
-import Contact from "./components/main/Contact"
+import Contact from "./components/main/Contact";
 import myStore from "./redux/store";
 import { Provider } from "react-redux";
 
@@ -20,15 +20,18 @@ function App() {
       <Provider store={myStore}>
         <BrowserRouter>
           <Header />
-          <Routes>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/archives" element={<Archives />}></Route>
-            <Route path="/career" element={<Career />}></Route>
-            <Route path="/events" element={<Events />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
-          </Routes>
+          <div style={{ paddingBottom: "2rem" }}>
+            <Routes>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/archives" element={<Archives />}></Route>
+              <Route path="/career" element={<Career />}></Route>
+              <Route path="/events" element={<Events />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+            </Routes>
+          </div>
+
           <Footer />
         </BrowserRouter>
       </Provider>
--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -1,8 +1,65 @@
 import React, { Component } from "react";
+import logo from "../../assets/react.png";
+import { BiCopyright } from "react-icons/bi";
+import { BsFacebook, BsTwitter, BsLinkedin, BsTelegram } from "react-icons/bs";
 
 export class Footer extends Component {
   render() {
-    return <div>Footer</div>;
+    return (
+      <div>
+        <div className="footer_container">
+          <div className="footer_columns">
+            <div className="footer_items">
+              <img className="footer_logo" src={logo} alt="Logo" />
+              <h4>Tech React</h4>
+              <p>
+                <BiCopyright />
+                <a
+                  style={{
+                    marginLeft: "5px",
+                    textDecoration: "none",
+                    color: "white",
+                    fontSize: "medium",
+                  }}
+                  href="https://github.com/scottishsummer98"
+                >
+                  scottishsummer98
+                </a>
+              </p>
+            </div>
+            <div className="footer_items">
+              <h4>Company</h4>
+              <h6>Archives</h6>
+              <h6>Career</h6>
+              <h6>Events</h6>
+              <h6>Contact</h6>
+            </div>
+            <div className="footer_items">
+              <h4>Further Information</h4>
+              <h6>Terms & Condition</h6>
+              <h6>Privacy Policy</h6>
+            </div>
+            <div className="footer_items">
+              <h4>Follow Us</h4>
+              <div className="footer_socials">
+                <h6>
+                  <BsFacebook />
+                </h6>
+                <h6>
+                  <BsTwitter />
+                </h6>
+                <h6>
+                  <BsLinkedin />
+                </h6>
+                <h6>
+                  <BsTelegram />
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
