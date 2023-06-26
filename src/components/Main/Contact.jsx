917a4e61e180b6e9cf60cdf4410005a26414e161
scottishsummer98 <samiremail98@gmail.com>
2023-06-26 14:06:26 +0600

    'reactreduxforminit'

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,65 +1,33 @@
 import React, { Component } from "react";
-import { Form, Row, Col, FormGroup, Label, Input, Button } from "reactstrap";
-
+import { Row, Col, FormGroup, Label, Button } from "reactstrap";
+import { LocalForm, Control, Errors } from "react-redux-form";
 export class Contact extends Component {
-  constructor() {
-    super();
-    this.state = {
-      fullname: "",
-      email: "",
-      contactno: "",
-      sex: "",
-      address: "",
-      city: "",
-      zip: "",
-      inquiry: "",
-      inquiryOtherDetails: "",
-      termcond: false,
-    };
-    this.handleFormInput = this.handleFormInput.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-  handleFormInput = (event) => {
-    const name = event.target.name;
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    this.setState({
-      [name]: value,
-    });
-  };
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
+  handleFormSubmit = (values) => {
+    console.log(values);
   };
   render() {
     document.title = "Contact";
     return (
       <div>
-        <Form onSubmit={this.handleFormSubmit}>
+        <LocalForm onSubmit={(values) => this.handleFormSubmit(values)}>
           <Row>
             <Col md={6}>
               <FormGroup>
                 <Label for="fullname">Name</Label>
-                <Input
-                  id="fullname"
-                  name="fullname"
+                <Control.text
+                  className="form-control"
+                  model=".fullname"
                   placeholder="Enter Full Name"
-                  type="text"
-                  onChange={this.handleFormInput}
                 />
               </FormGroup>
             </Col>
             <Col md={6}>
               <FormGroup>
                 <Label for="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
+                <Control.text
+                  className="form-control"
+                  model=".email"
                   placeholder="Enter Email"
-                  type="email"
-                  onChange={this.handleFormInput}
                 />
               </FormGroup>
             </Col>
@@ -68,107 +36,102 @@ export class Contact extends Component {
             <Col md={6}>
               <FormGroup>
                 <Label for="contactno">Contact No.</Label>
-                <Input
-                  id="contactno"
-                  name="contactno"
+                <Control.text
+                  className="form-control"
+                  model=".contactno"
                   placeholder="Enter Contact No."
-                  type="text"
-                  onChange={this.handleFormInput}
                 />
               </FormGroup>
             </Col>
             <Col md={6}>
               <FormGroup>
                 <Label for="sex">Sex</Label>
-                <Input
-                  id="sex"
-                  name="sex"
-                  type="select"
-                  onChange={this.handleFormInput}
-                >
+                <Control.select className="form-control" model=".sex">
                   <option defaultChecked>Select sex</option>
                   <option>Male</option>
                   <option>Female</option>
                   <option>Others</option>
-                </Input>
+                </Control.select>
               </FormGroup>
             </Col>
           </Row>
           <FormGroup>
             <Label for="address">Address</Label>
-            <Input
-              id="address"
-              name="address"
+            <Control.textarea
+              className="form-control"
+              model=".address"
               placeholder="Enter Address"
-              onChange={this.handleFormInput}
+              rows="2"
             />
           </FormGroup>
           <Row>
             <Col md={6}>
               <FormGroup>
                 <Label for="city">City</Label>
-                <Input id="city" name="city" onChange={this.handleFormInput} />
+                <Control.text
+                  className="form-control"
+                  model=".city"
+                  placeholder="Enter City"
+                />
               </FormGroup>
             </Col>
             <Col md={6}>
               <FormGroup>
                 <Label for="zip">Zip</Label>
-                <Input id="zip" name="zip" onChange={this.handleFormInput} />
+                <Control.text
+                  className="form-control"
+                  model=".zip"
+                  placeholder="Enter Zip"
+                />
               </FormGroup>
             </Col>
           </Row>
           <FormGroup tag="fieldset">
             <legend>What is your inquiry about?</legend>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="NewsTips"
               />
               <Label check>News Tips</Label>
             </FormGroup>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="EventRelatedInquiries"
               />
               <Label check>Events Related Inquiries</Label>
             </FormGroup>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="Advertising_Sponsorships"
               />
               <Label check>Advertising & Sponsorships</Label>
             </FormGroup>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="ErrorReports"
               />
               <Label check>Error Reports</Label>
             </FormGroup>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="CareerHub"
               />
               <Label check>Career Hub</Label>
             </FormGroup>
             <FormGroup check>
-              <Input
-                name="inquiry"
-                type="radio"
-                onChange={this.handleFormInput}
+              <Control.radio
+                className="form-check-input"
+                model=".inquiry"
                 value="Others"
               />
               <Label check>Others</Label>
@@ -176,28 +139,21 @@ export class Contact extends Component {
           </FormGroup>
           <FormGroup>
             <Label for="inquiryOtherDetails">Details</Label>
-            <Input
-              id="inquiryOtherDetails"
-              name="inquiryOtherDetails"
-              type="textarea"
+            <Control.textarea
+              className="form-control"
+              model=".inquiryOtherDetails"
               rows="4"
-              onChange={this.handleFormInput}
             />
           </FormGroup>
           <FormGroup check>
-            <Input
-              id="termcond"
-              name="termcond"
-              type="checkbox"
-              onChange={this.handleFormInput}
-            />
+            <Control.checkbox className="form-check-input" model=".termcond" />
             <Label check for="termcond">
               I agree with the terms and conditions
             </Label>
           </FormGroup>
           <br />
           <Button>Send</Button>
-        </Form>
+        </LocalForm>
       </div>
     );
   }
